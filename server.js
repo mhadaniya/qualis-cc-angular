5e9c3b47300d6a12641594a37e0f232a97014c26
Mario H. Adaniya <mhadaniya@gmail.com>
2016-11-02 02:25:23 -0200

    alterando a porta do app

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require('http')
     ,app = require('./config/express')
     db = require('./config/database');
 
-http.createServer(app).listen(3001, function() {
+http.createServer(app).listen(5000, function() {
     console.log('Server listening... : ' + this.address().port);
 });
