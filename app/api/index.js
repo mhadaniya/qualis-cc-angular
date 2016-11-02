9f6c2d63ec875b31fdd77b6ee73ab857ceece6b8
Mario H. Adaniya <mhadaniya@gmail.com>
2016-11-02 02:19:22 -0200

    Ordenando a lista pelo estrato

--- app/api/index.js
+++ app/api/index.js
@@ -3,7 +3,7 @@ var db = require('../../config/database');
 var api = {}
 
 api.lista = function(req, res) {
-    db.find({}).sort({_id: 1}).exec(function(err, doc) {
+    db.find({}).sort({estrato: 1}).exec(function(err, doc) {
         if (err) return console.log(err);
         res.json(doc);
     });
